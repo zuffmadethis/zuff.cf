d6b63b9f77424333f2cbe1051aceaa23137a83af
zuffmadethis <108103764+zuffmadethis@users.noreply.github.com>
2022-06-23 20:42:41 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+</body>
+</html>
+<body>
+<head>
+<meta charset="UTF-8">
+<title>Zuff | Webpage</title>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css'><link rel="stylesheet" href="css/style0.css">
+</body>
+</head>
+<body>
+
+<section>
+<ul class="icon-list">
+<li class="icon-item">
+<a href="https://github.com/zuffmadethis" class="icon-link"><i class="fab fa-github"></i></a>
+</li>
+<li class="icon-item">
+<a href="contacts/discord.html" class="icon-link"><i class="fab fa-discord"></i></a>
+</li>
+<li class="icon-item">
+<a href="https://discord.gg/nM6Cukp3eS" class="icon-link"><i class="fab fa-tiktok"></i></a>
+</li>
+<li class="icon-item">
+<a href="https://www.youtube.com/channel/UC3NCJCrk6P4HMFCm_AvXioQ" class="icon-link"><i class="fab fa-youtube"></i></a>
+</li>
+<li class="icon-item">
+<a href="contacts/donate.html" class="icon-link"><i class="fab fa-bitcoin"></i></a>
+</li>
+</ul>
+</section>
+
+</body>
+</html>
